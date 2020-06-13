5052693d161fbcbfcea4a061b8897caeeae0b248
Felix Pacheco <devfex.19@gmail.com>
2020-06-13 14:12:28 -0400

    serving style.css in index

--- html/server.js
+++ html/server.js
@@ -1,20 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/statics',express.static('statics'));
+// use inserta un nuevo middlelware en el stack de middlewares
 
 app.get("/", (req, res) => {
   res.sendFile('index.html',{
     root: __dirname
   });
-  // res.send(__dirname);
 });
-
-// // POST
-// app.post("/saludo", (req, res) => {
-//   res.send(`Hola ${req.body.name}`);
-// });
 
 app.listen(3000);
