f9daa5d654e99d22a0c0869c473dec5b05dfb60c
Felix Pacheco <devfex.19@gmail.com>
2020-06-13 13:42:52 -0400

    implementing method post to send data to server

--- primer-server/express.js
+++ primer-server/express.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+// GET
 app.get( '/saludo', (req,res) => {
   res.send(`Hola ${req.query.name}`)
+})
+
+// POST
+app.post( '/saludo', (req,res) => {
+  res.send(`Hola ${req.body.name}`)
 })
 
 app.listen(3000);
