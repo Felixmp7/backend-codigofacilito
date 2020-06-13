19f051a7d8717e96126e00f4bc4515a7a02a5cc4
Felix Pacheco <devfex.19@gmail.com>
2020-06-13 14:37:13 -0400

    testing caches config

--- motores-vista/server.js
+++ motores-vista/server.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use('/statics',express.static('statics'));
+app.use('/statics',express.static('statics', {
+  etag: false,
+  maxAge: '5h'
+}));
 // use inserta un nuevo middlelware en el stack de middlewares
 
 app.get("/", (req, res) => {
